8cbd4f267795025a811baa9240240c682af54719
nvizcarra <nicolasvizcarra64@gmail.com>
2021-05-24 20:10:57 -0300

    Test modify scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -24,48 +24,48 @@ function readText(e) {
 }
 
 // Submit Event
-const contactForm = document.querySelector('#contact__form');
-contactForm.addEventListener('submit', function(evento) {
-    evento.preventDefault();
+// const contactForm = document.querySelector('#contact__form');
+// contactForm.addEventListener('submit', function(evento) {
+//     evento.preventDefault();
 
-    // Form validation with destructuring
-    const { inputName, inputEmail, inputMessage } = formData;
+//     // Form validation with destructuring
+//     const { inputName, inputEmail, inputMessage } = formData;
 
-    if(inputName === '' || inputEmail === '' || inputMessage === '') {
-        showError('Please complete all fields');
+//     if(inputName === '' || inputEmail === '' || inputMessage === '') {
+//         showError('Please complete all fields');
 
-        return;
-    } else return messageSent('Your message has been sent');
+//         return;
+//     } else return messageSent('Your message has been sent');
 
-    // Form Sent
+//     // Form Sent
 
-    console.log('Form sent');
-});
+//     console.log('Form sent');
+// });
 
-// Show error function
-function showError(mensaje) {
-    const error = document.createElement('P');
-    error.textContent = mensaje;
-    error.classList.add('error');
+// // Show error function
+// function showError(mensaje) {
+//     const error = document.createElement('P');
+//     error.textContent = mensaje;
+//     error.classList.add('error');
 
-    contactForm.appendChild(error);
+//     contactForm.appendChild(error);
 
-    // Error fadeout
-    setTimeout(() => {
-        error.remove();
-    }, 3000);
-}
+//     // Error fadeout
+//     setTimeout(() => {
+//         error.remove();
+//     }, 3000);
+// }
 
-// Show success message function
-function messageSent(mensaje) {
-    const success = document.createElement('P');
-    success.textContent = mensaje;
-    success.classList.add('success');
+// // Show success message function
+// function messageSent(mensaje) {
+//     const success = document.createElement('P');
+//     success.textContent = mensaje;
+//     success.classList.add('success');
 
-    contactForm.appendChild(success);
+//     contactForm.appendChild(success);
 
-     // Message fadeout
-     setTimeout(() => {
-        success.remove();
-    }, 4000);
-}
+//      // Message fadeout
+//      setTimeout(() => {
+//         success.remove();
+//     }, 4000);
+// }
